eaa3d5201203bed8735f2c0b9408941da88499f8
Lisa Zhao <84100023+lisazhao30@users.noreply.github.com>
2021-08-28 15:46:46 -0400

    worked on the form

---
+++ interests-form-page.js
@@ -0,0 +1,7 @@
+var Form1 = document.getElementById("Form1");
+var Form2 = document.getElementById("Form2");
+var Form3 = document.getElementById("Form3");
+
+var Next1 = document.getElementById("Next1");
+var Next2 = document.getElementById("Next2");
+
