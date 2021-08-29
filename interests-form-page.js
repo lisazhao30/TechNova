ce2506b876f67ca72b6b2d1c714363c4ae65c516
Lisa Zhao <84100023+lisazhao30@users.noreply.github.com>
2021-08-28 22:49:26 -0400

    did stuff

--- interests-form-page.js
+++ interests-form-page.js
@@ -4,4 +4,19 @@ var Form3 = document.getElementById("Form3");
 
 var Next1 = document.getElementById("Next1");
 var Next2 = document.getElementById("Next2");
+var Back1 = document.getElementById("Back1");
 
+function myFunction(){
+    document.getElementById("Form1").style.left = "-450px"
+    document.getElementById("Form2").style.left = "40px"
+}
+
+Next1.onclick = function(){
+    Form1.style.left = "-450px";
+    Form2.style.left = "40px";
+}
+
+Back1.onclick = function(){
+    Form1.style.left = "40px";
+    Form2.style.left = "450px";
+}
